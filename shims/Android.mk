8cac1235fd19b966467669862a4427c2347bd245
elpaablo <donopwhile1@gmail.com>
2021-04-11 17:51:31 +0100

    nuke libmedia_jni_shim and MiuiCamera rro overlays

--- shims/Android.mk
+++ shims/Android.mk
@@ -7,9 +7,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY) 
 
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libmedia
-LOCAL_SRC_FILES := libmedia_jni.cpp
-LOCAL_MODULE := libmedia_jni_shim
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
--- shims/libmedia_jni.cpp
+++
@@ -1,5 +0,0 @@
-static bool _ZN7android11CameraUtils23isCameraServiceDisabledEv()
-{
-  return false;
-}
-
